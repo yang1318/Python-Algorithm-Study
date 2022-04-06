f5104771d06dd669cd3cb18e318326ef3c5cc30e
seona.Yang <75965560+yang1318@users.noreply.github.com>
2022-04-07 00:42:33 +0900

    [CodeUp]파이썬 기초-종합 (코드 수정-for문 3개=>for문 2개)
    기존의 코드에서는 가로, 세로 for문을 따로 돌렸었는데, 둘다 0부터 18까지 19번 돌아야된다는 점에서 합칠 수 있었기에 for문 합침.

--- yang1318/CodeUp/6096_바둑판 십자 뒤집기.py
+++ yang1318/CodeUp/6096_바둑판 십자 뒤집기.py
@@ -7,19 +7,14 @@ n = int(input())
 for i in range(n):
   x, y = map(lambda a:int(a)-1, input().split())
   for k in range(19):
-    if k==x:
-      continue
     if board[k][y] == 1:
       board[k][y] = 0
     else:
       board[k][y] = 1
-  for j in range(19):
-    if j==y:
-      continue
-    if board[x][j] == 1:
-      board[x][j] = 0
+    if board[x][k] == 1:
+      board[x][k] = 0
     else:
-      board[x][j] = 1
+      board[x][k] = 1
 
 for i in range(19):
   for j in range(19):
