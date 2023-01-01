5d26a14e9c98f94bdaf534afbd2c32f8b485260a
yang1318 <amy1353@naver.com>
2023-01-02 04:34:01 +0900

    [Baekjoon]다익스트라(코드수정)

--- yang1318/Baekjoon/1916_최소비용 구하기.py
+++ yang1318/Baekjoon/1916_최소비용 구하기.py
@@ -3,7 +3,7 @@ import sys
 INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
 
 def ans():
-    #입력 및 초기화
+    # 입력 및 초기화
     n = int(sys.stdin.readline())
     m = int(sys.stdin.readline())
     bus = [[] for _ in range(n+1)]
@@ -11,20 +11,23 @@ def ans():
         a, b, c = map(int, sys.stdin.readline().split()) #출발, 도착, 비용
         bus[a].append((b,c))
     start, end = map(int, sys.stdin.readline().split())
-    distance = [INF] * (n + 1)
+
+    #다익스트라 알고리즘
     q = []
     heapq.heappush(q, (0, start))
+    distance = [INF] * (n + 1)
     distance[start] = 0
-    while q: # 큐가 비어있지 않다면
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+    while q:
+        cost, now = heapq.heappop(q)
+        if distance[now] < cost:
             continue
+        if now == end:
+            print(cost)
+            break
         for i in bus[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-    
-    print(distance[end])
+            c = cost + i[1]
+            if c < distance[i[0]]:
+                distance[i[0]] = c
+                heapq.heappush(q, (c, i[0]))
 
 ans()
