336980f67ba7fd1d9e195eeb480da40d82897f8c
yang1318 <amy1353@naver.com>
2023-01-05 02:31:13 +0900

    [Baekjoon]수학(코드수정-정렬을 이용한 시간복잡도 개선)

--- yang1318/Baekjoon/2798_블랙잭.py
+++ yang1318/Baekjoon/2798_블랙잭.py
@@ -1,9 +1,12 @@
 n, m = map(int, input().split())
-card = list(map(int, input().split()))
-ans=0
-for i in range(n):
-    for j in range(i+1,n):
+card = sorted(list(map(int, input().split())), reverse=True)
+ans = 0
+for i in range(n-2):
+    for j in range(i+1,n-1):
         for k in range(j+1,n):
-            if card[i]+card[j]+card[k] <= m and m-ans>m-card[i]-card[j]-card[k]:
-                ans=card[i]+card[j]+card[k]
+            s = card[i]+card[j]+card[k]
+            if s<=m:
+                if ans<s:
+                    ans=s
+                break
 print(ans)
