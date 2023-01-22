113aae5094bf70ac280491e1cffe0daeb7ac6f52
yang1318 <amy1353@naver.com>
2023-01-22 23:37:47 +0900

    [Baekjoon]기하

---
+++ yang1318/Baekjoon/1002_터렛.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = ((x1-x2)**2 + (y1-y2)**2)**(1/2)
+    if distance == 0 and r1 == r2:
+        print(-1)
+    elif distance == r1 + r2 or distance == abs(r1-r2):
+        print(1)
+    elif abs(r1 - r2) < distance < r1 + r2:
+        print(2)
+    else:
+        print(0)
---
+++ yang1318/Baekjoon/3053_택시 기하학.py
@@ -0,0 +1,5 @@
+import math
+R = int(input())
+euclid = R*R*math.pi
+taxi = ((2*R)**2)*0.5 # == 2*(r**2)
+print(f"{euclid}\n{taxi}")
